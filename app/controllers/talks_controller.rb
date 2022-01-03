d6988999a994ddd341a7ca8d153c8ebd589fc601
szakisip <ssg2012.yamato.s@gmail.com>
2022-01-04 04:30:55 +0900

    エピソードトーク投稿ビュー作成、talksコントローラー記載追加、ルーティング追加

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,4 +1,13 @@
 class TalksController < ApplicationController
   def index
+    # @talks = Talk.includes(:user).order("created_at DESC")
   end
+
+  def new
+    # @talk = Talk.new
+  end
+
+  def create
+  end
+
 end
